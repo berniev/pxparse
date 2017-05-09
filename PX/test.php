f783a40b2bd524f53edab341635e10e6d0d1901d
berniev <berniev@bje.com.au>
2017-05-09 17:55:37 +1000

    tidied up

--- PX/test.php
+++ PX/test.php
@@ -28,7 +28,6 @@ include 'loader.php';
 $dB = 'testparse';
 $tableNames = [];
 $path = './testfiles/';
-//$path = '/Users/bernievanthof/archives/dos_19_apr_2016/DRIVE_C/PDOXDATA/';
 $parser = new ParseTables($tableNames, $path);
 $parser->setDestDb($dB);
 $parser->Parse();
